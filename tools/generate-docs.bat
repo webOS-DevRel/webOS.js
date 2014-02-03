2851da1eb92e3e7ca941f6eec4e8750daa3c3fb1
Jason Robitaille <jason@canuckcoding.ca>
2014-02-03 03:55:47 -0600

    Updated and working. Properly generates documents in a docs folder, auto-downloading jsdoc and creating an altered template to use in tools/util/docs-template using the custom-publish.js.

--- tools/generate-docs.bat
+++ tools/generate-docs.bat
@@ -12,10 +12,21 @@ if not exist "%WEBOSJS%node_modules\jsdoc\jsdoc.js" (
 	(echo.)
 )
 
-rmdir /S /Q "%WEBOSJS%docs"
+if not exist "%WEBOSJS%tools\util\docs-template\publish.js" (
+	xcopy "%WEBOSJS%node_modules\jsdoc\templates\default\*" "%WEBOSJS%tools\util\docs-template\" /q /s /e /y > nul
+	copy "%WEBOSJS%tools\util\docs-template\custom-publish.js" "%WEBOSJS%tools\util\docs-template\publish.js" /Y > nul
+	copy "%WEBOSJS%node_modules\jsdoc\LICENSE.md" "%WEBOSJS%tools\util\docs-template\LICENSE.md" /Y > nul
+)
+
+if exist "%WEBOSJS%docs" rmdir "%WEBOSJS%docs" /S /Q 
 mkdir "%WEBOSJS%docs"
 
 echo Generating webOS.js Documentation...
 
-node "%WEBOSJS%node_modules\jsdoc\jsdoc.js" "%WEBOSJS%src" -d "%WEBOSJS%docs"
-if %errorlevel% EQU 0 echo Successfully built to %WEBOSJS%docs
+node "%WEBOSJS%tools\util\jsdoc-build.js" "%WEBOSJS%src" "%WEBOSJS%docs\webOS.js"
+node "%WEBOSJS%node_modules\jsdoc\jsdoc.js" "%WEBOSJS%docs\webOS.js" -d "%WEBOSJS%docs" -t "%WEBOSJS%tools\util\docs-template"
+if %errorlevel% EQU 0 (
+	echo Successfully built to %WEBOSJS%docs
+	copy "%WEBOSJS%tools\util\docs-template\index.html" "%WEBOSJS%docs\index.html" /Y > nul
+)
+del "%WEBOSJS%docs\webOS.js" /Q > nul
--- tools/generate-docs.sh
+++ tools/generate-docs.sh
@@ -12,18 +12,26 @@ if command -v node >/dev/null 2>&1; then
 			mkdir -p "$WEBOSJS/node_modules"
 		fi
 		npm install jsdoc --prefix "$WEBOSJS/node_modules"
+		
 		echo " "
+	fi
+	if [ ! -f "$WEBOSJS/tools/util/docs-template/publish.js" ]  ; then
+		cp -fr "$WEBOSJS/node_modules/jsdoc/templates/default/"* "$WEBOSJS/tools/util/docs-template/"
+		cp -f "$WEBOSJS/tools/util/docs-template/custom-publish.js" "$WEBOSJS/tools/util/docs-template/publish.js"
+		cp -f "$WEBOSJS/node_modules/jsdoc/LICENSE.md" "$WEBOSJS/tools/util/docs-template/LICENSE.md"
 	fi
 	
 	rm -fr "$WEBOSJS/docs"
 	mkdir -p "$WEBOSJS/docs"	
 	
 	echo "Generating webOS.js Documentation..."
-	
-	node "$WEBOSJS/node_modules/jsdoc/jsdoc.js" "$WEBOSJS/src" -d "$WEBOSJS/docs"
+	node "$WEBOSJS/tools/util/jsdoc-build.js" "$WEBOSJS/src" "$WEBOSJS/docs/webOS.js"
+	node "$WEBOSJS/node_modules/jsdoc/jsdoc.js" "$WEBOSJS/docs/webOS.js" -d "$WEBOSJS/docs" -t "$WEBOSJS/tools/util/docs-template/"
 	if [ $? -eq 0 ] ; then
 		echo "Successfully built to $WEBOSJS/docs"
+		cp -f "$WEBOSJS/tools/util/docs-template/index.html" "$WEBOSJS/docs/index.html"
 	fi
+	rm -f "$WEBOSJS/docs/webOS.js"
 else
 	echo "No node found in path"
 	exit 1
