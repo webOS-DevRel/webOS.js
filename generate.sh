1dd5f6c1860051256caf6e9d61764260338d4670
Jason Robitaille <jason.robitaille@lge.com>
2015-06-12 16:19:24 -0700

    Updated layout

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 cd webOS.js
 npm install .
 gulp docs
-mv docs/* ..
+cp -fr docs/* ..
 rm -fr docs
 rm -fr node_modules
 cd ..
-cp webOS.html index.html
+sed -e 's/        webOS//' -e 's/Namespace: webOS/<img src="logo.png" style="margin: 0 0 -40px -20px;"\/>/' -e 's/<h3 class="subsection-title">Namespaces<\/h3>/<h1 style="margin-left:-10px">window.webOS<\/h1><h3 class="subsection-title">Namespaces<\/h3>/'  webOS.html > index.html
